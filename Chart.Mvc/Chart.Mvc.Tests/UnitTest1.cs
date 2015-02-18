b2a391dbf6901b84927ee886332c5fd51785355a
Martino Bordin <info@martinobordin.it>
2015-02-18 18:57:55 +0100

    ObjectExtensions for Json Serialization

---
+++ Chart.Mvc/Chart.Mvc.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Chart.Mvc.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chart.Mvc.Tests
+{
+    [TestClass]
+    public class ObjectExtensionsTests
+    {
+        [TestMethod]
+        public void CanSerializeToJson()
+        {
+            var json = DateTime.Now.ToJson();
+            Assert.IsFalse(string.IsNullOrWhiteSpace(json));
+        }
+    }
+}
---
+++ Chart.Mvc/Chart.Mvc/Extensions/ObjectExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Chart.Mvc.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// The json serializer settings.
+        /// </summary>
+        private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            NullValueHandling = NullValueHandling.Ignore
+        };
+
+        /// <summary>
+        /// The to json.
+        /// </summary>
+        /// <param name="obj">
+        /// The obj.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public static string ToJson<T>(this T obj)
+        {
+            return JsonConvert.SerializeObject(obj, JsonSerializerSettings);
+        }
+    }
+}
